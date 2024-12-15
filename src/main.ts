637f23442ef97a00196e5341e2859deccb3c6467
alonecat <htchtc052@gmail.com>
2024-12-16 01:29:08 +0300

    Изменено workflow

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import App from './App.vue'
 import router from "./router";
 import pinia from "./stores";
 
-console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
+console.log("VITE_API_BASE_URL:",import.meta.env.VITE_API_BASE_URL);
 
 createApp(App).use(router).use(pinia).mount('#app')
